f47c028e2a813cf37cb27931e75cd446b6f9d349
paniszn <panisznorbert@gmail.com>
2019-01-14 16:59:04 +0100

    adatbázisba ír és olvas is

--- src/main/java/panisz/norbert/simongumis/view/MainView.java
+++ src/main/java/panisz/norbert/simongumis/view/MainView.java
@@ -12,7 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import panisz.norbert.simongumis.entities.UgyfelEntity;
 import panisz.norbert.simongumis.repositories.UgyfelRepository;
 
-
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Set;
 
 
 @Route
@@ -22,20 +24,57 @@ public class MainView extends VerticalLayout implements KeyNotifier {
     TextField telefon = new TextField("Telefon");
     TextField email = new TextField("E-mail");
     Button ment = new Button("Mentés", VaadinIcon.CHECK.create());
+    Button betolt = new Button("Betölt", VaadinIcon.CHECK.create());
+    Button szamol = new Button("Számol", VaadinIcon.CHECK.create());
+
+    TextField db = new TextField("DB");
+    TextField nev2 = new TextField("Név");
+    TextField telefon2 = new TextField("Telefon");
+    TextField email2 = new TextField("E-mail");
 
     Binder<UgyfelEntity> binder = new Binder<>(UgyfelEntity.class);
 
     @Autowired
     public MainView(UgyfelRepository ugyfelRepository) {
-        add(nev, telefon, email, ment);
+        add(nev, telefon, email, ment, betolt, db, szamol);
 
         binder.bindInstanceFields(this);
 
         setSpacing(true);
 
-        addKeyPressListener(Key.ENTER, e -> ugyfelRepository.save(ugyfel));
+        addKeyPressListener(Key.ENTER, e -> ugyfelRepository.save(createUgyfel(nev.getValue(), telefon.getValue(), email.getValue())));
+        ment.addClickListener(e -> ugyfelRepository.save(createUgyfel(nev.getValue(), telefon.getValue(), email.getValue())));
 
+        betolt.addClickListener(e -> betolt(ugyfelRepository.findAll()));
+
+        szamol.addClickListener(e -> db.setValue(new Integer(ugyfelRepository.findAll().size()).toString()));
 
 
     }
+
+    private void szamol(){
+
+    }
+
+    private void betolt(Collection<UgyfelEntity> lista){
+        ArrayList<UgyfelEntity> ugyfelek = new ArrayList();
+        ugyfelek.addAll(lista);
+        add(nev2, telefon2, email2);
+        UgyfelEntity ugyfel=ugyfelek.get(ugyfelek.size()-1);
+        nev2.setValue(ugyfel.getNev());
+        telefon2.setValue(ugyfel.getTelefon());
+        email2.setValue(ugyfel.getEmail());
+
+    }
+
+    private UgyfelEntity createUgyfel (String nev, String telefon, String email){
+        UgyfelEntity ugyfel= new UgyfelEntity();
+        ugyfel.setTelefon(telefon);
+        ugyfel.setNev(nev);
+        ugyfel.setEmail(email);
+
+        return ugyfel;
+    }
+
+
 }
