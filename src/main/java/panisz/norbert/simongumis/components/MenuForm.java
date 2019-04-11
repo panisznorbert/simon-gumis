40d343d2002477320cee430635ef3d7c67de16e2
paniszn <panisznorbert@gmail.com>
2019-04-11 15:12:12 +0200

    javítás

--- src/main/java/panisz/norbert/simongumis/components/MenuForm.java
+++ src/main/java/panisz/norbert/simongumis/components/MenuForm.java
@@ -6,14 +6,17 @@ import com.vaadin.flow.component.applayout.AppLayoutMenu;
 import com.vaadin.flow.component.applayout.AppLayoutMenuItem;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import panisz.norbert.simongumis.LoggerExample;
 import panisz.norbert.simongumis.spring.SpringApplication;
+
+import java.util.logging.Logger;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
-public class MenuForm extends HorizontalLayout {
+class MenuForm extends VerticalLayout {
     private AppLayout appLayout = new AppLayout();
     private AppLayoutMenuItem rendelesek = new AppLayoutMenuItem(VaadinIcon.BOOK_DOLLAR.create(), "Rendelések", e -> UI.getCurrent().navigate("rendelesek"));
     private AppLayoutMenuItem lefoglaltIdopontok = new AppLayoutMenuItem(VaadinIcon.CALENDAR_USER.create(), "Lefoglalt időpontok", e -> UI.getCurrent().navigate("lefoglalt_idopontok"));
@@ -24,7 +27,9 @@ public class MenuForm extends HorizontalLayout {
     private AppLayoutMenuItem szolgaltatasok = new AppLayoutMenuItem(VaadinIcon.TOOLS.create(), "Szolgáltatások", e -> UI.getCurrent().navigate("szolgaltatasok"));
     private AppLayoutMenuItem kezdolap = new AppLayoutMenuItem(VaadinIcon.HOME.create(), "Kezdőlap", e -> UI.getCurrent().navigate(""));
 
-    public MenuForm(){
+    private final static Logger LOGGER = Logger.getLogger(LoggerExample.class.getName());
+
+    MenuForm(){
         AppLayoutMenu menu = appLayout.createMenu();
 
         if(SpringApplication.getRendelesAzon()!=null){
@@ -42,12 +47,15 @@ public class MenuForm extends HorizontalLayout {
         menuElemeinekBeallitasa(menu, rendelesek);
         add(appLayout);
 
+        this.setHeight("60px");
+        this.appLayout.getElement().getStyle().set("height", "60px");
+        this.getStyle().set("z-index", "99999");
+
     }
 
     private void menuElemeinekBeallitasa(AppLayoutMenu menu, AppLayoutMenuItem menuItem) {
         menuItem.getElement().setAttribute("theme", "icon-on-top");
         menu.addMenuItem(menuItem);
     }
-
 
 }
