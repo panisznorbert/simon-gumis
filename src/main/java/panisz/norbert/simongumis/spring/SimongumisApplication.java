b4d692a177b696e803f7089e21af1f62adb4047a
panisz.norbert <panisznorbert@gmail.com>
2019-05-02 23:23:28 +0200

    felesleg kiszedve

--- src/main/java/panisz/norbert/simongumis/spring/SimongumisApplication.java
+++ src/main/java/panisz/norbert/simongumis/spring/SimongumisApplication.java
@@ -1,29 +1,18 @@
 package panisz.norbert.simongumis.spring;
 
 import com.vaadin.flow.spring.annotation.EnableVaadin;
-import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import panisz.norbert.simongumis.services.RendelesService;
 
 @EnableVaadin("panisz.norbert.simongumis")
 @SpringBootApplication(scanBasePackages = "panisz.norbert.simongumis")
 @EntityScan(basePackages = "panisz.norbert.simongumis.entities")
 @EnableJpaRepositories(basePackages = "panisz.norbert.simongumis")
 @EnableTransactionManagement
-@Data
 public class SimongumisApplication {
-
-    public static RendelesService getRendelesService() {
-        return rendelesService;
-    }
-
-    @Autowired
-    private static RendelesService rendelesService;
 
     public static void main(String[] args) {
         SpringApplication.run(SimongumisApplication.class, args);
