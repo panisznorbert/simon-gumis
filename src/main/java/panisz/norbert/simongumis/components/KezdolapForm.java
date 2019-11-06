889b59dbc3c8b5e9c3d6985b6b69a8c6c4fb3eb6
panisznorbert@gmail.com <Pnorbi22>
2019-11-06 17:48:07 +0100

    File feltöltés kialakítása kezdet

--- src/main/java/panisz/norbert/simongumis/components/KezdolapForm.java
+++ src/main/java/panisz/norbert/simongumis/components/KezdolapForm.java
@@ -2,17 +2,23 @@ package panisz.norbert.simongumis.components;
 
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.upload.Upload;
+import com.vaadin.flow.component.upload.receivers.FileBuffer;
 import com.vaadin.flow.spring.annotation.UIScope;
 import org.springframework.stereotype.Component;
+
+import java.io.InputStream;
+
 
 @UIScope
 @Component
 public class KezdolapForm extends VerticalLayout {
 
-    VerticalLayout alap = new VerticalLayout();
+
 
     public KezdolapForm(){
 
+        VerticalLayout alap = new VerticalLayout();
 
         Image szorolap = new Image("images/borito.png", "");
 
@@ -22,7 +28,17 @@ public class KezdolapForm extends VerticalLayout {
 
         alap.add(szorolap);
 
-        this.add(alap);
+        FileBuffer fileBuffer = new FileBuffer();
+        Upload upload = new Upload(fileBuffer);
+        upload.addFinishedListener(e -> {
+            InputStream inputStream =
+                    fileBuffer.getInputStream();
+            // read the contents of the buffered file
+            // from inputStream
+        });
+
+        this.add(alap, upload);
 
     }
+
 }
