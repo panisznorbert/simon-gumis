3c8598355c723e174731ead7e6cafe6342e3ff79
panisznorbert@gmail.com <Pnorbi22>
2019-12-11 12:23:24 +0100

    Beállítások sorelválasztása

--- src/main/java/panisz/norbert/simongumis/components/BeallitasokForm.java
+++ src/main/java/panisz/norbert/simongumis/components/BeallitasokForm.java
@@ -1,12 +1,13 @@
 package panisz.norbert.simongumis.components;
 
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.spring.annotation.UIScope;
 import org.springframework.stereotype.Component;
 import panisz.norbert.simongumis.services.KezdolapTartalomService;
 import panisz.norbert.simongumis.services.NyitvatartasService;
 
-
+@StyleSheet("style.css")
 @UIScope
 @Component
 public class BeallitasokForm extends VerticalLayout {
@@ -14,7 +15,9 @@ public class BeallitasokForm extends VerticalLayout {
     public BeallitasokForm(NyitvatartasService nyitvatartasService, KezdolapTartalomService kezdolapTartalomService){
         VerticalLayout nyitvatartasModositasa = new NyitvatartasModositasa(nyitvatartasService);
         VerticalLayout kezdolapKepModositasa = new SzorolapModositasa(kezdolapTartalomService);
+        kezdolapKepModositasa.addClassName("szegely");
         VerticalLayout kezdolapTartalmanakModositasa = new KezdolapTartalomKezelese(kezdolapTartalomService);
+        kezdolapTartalmanakModositasa.addClassName("szegely");
         add(nyitvatartasModositasa, kezdolapKepModositasa, kezdolapTartalmanakModositasa);
 
     }
