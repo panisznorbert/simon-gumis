d945087bf102e414f1eb1ae41b37023868edfa43
panisznorbert <panisznorbert@gmail.com>
2020-05-11 13:45:41 +0200

    jelszó base64 kódolva

--- src/main/java/panisz/norbert/simongumis/services/implement/AdminServiceImpl.java
+++ src/main/java/panisz/norbert/simongumis/services/implement/AdminServiceImpl.java
@@ -1,17 +1,19 @@
 package panisz.norbert.simongumis.services.implement;
 
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import panisz.norbert.simongumis.entities.AdminEntity;
 import panisz.norbert.simongumis.repositories.AdminRepository;
 import panisz.norbert.simongumis.services.AdminService;
-
 import java.util.List;
+import java.util.logging.Logger;
 
 @Service
 @Transactional
 public class AdminServiceImpl implements AdminService {
+    //private final static Logger LOGGER = Logger.getLogger(AdminServiceImpl.class.getName());
     @Autowired
     AdminRepository adminRepository;
 
@@ -22,7 +24,7 @@ public class AdminServiceImpl implements AdminService {
 
     @Override
     public AdminEntity adminraKeres(String nev, String jelszo) {
-        return adminRepository.findByNevAndJelszo(nev, jelszo);
+        return adminRepository.findByNevAndJelszo(nev, Base64.encodeBase64String(jelszo.getBytes()));
     }
 
     @Override
